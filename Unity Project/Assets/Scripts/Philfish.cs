c4ed84bb58fba18fb9f1c7f84ae5288cbed6639b
Lawrence-Stevens <lawrencestevens2001@gmail.com>
2014-08-25 12:29:59 -0700

    Fuck.Yes.

--- Unity Project/Assets/Scripts/Philfish.cs
+++ Unity Project/Assets/Scripts/Philfish.cs
@@ -30,4 +30,25 @@ public class Philfish : MonoBehaviour {
 	{
 		rigidbody2D.velocity = movement;
 	}
+	void OnCollisionEnter2D(Collision2D collision)
+	{
+		bool damagePlayer = false;
+
+		EnemyScript enemy = collision.gameObject.GetComponent<EnemyScript>();
+		if (enemy != null)
+		{
+
+			HealthScript enemyHealth = enemy.GetComponent<HealthScript>();
+			if (enemyHealth != null) enemyHealth.Damage(enemyHealth.hp);
+			
+			damagePlayer = true;
+		}
+
+		if (damagePlayer)
+		{
+			HealthScript playerHealth = this.GetComponent<HealthScript>();
+			if (playerHealth != null) playerHealth.Damage(1);
+		}
+	}
+
 }
